771518054b2c525f715e186e2f582adbdf2865fc
Paul Kinlan <paulkinlan@dhcp-172-16-20-149.lon.corp.google.com>
2010-12-06 20:49:46 +0000

    Bits and bobs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2010 Paul Kinlan.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.api import urlfetch
